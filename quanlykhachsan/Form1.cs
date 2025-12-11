3a060042dc56a0096699fec28b38b6cb623811a3
loc1802 <phucloc0125@gmail.com>
2025-12-11 11:36:55 +0700

    them tất cả vô Add project files.

---
+++ quanlykhachsan/Form1.cs
@@ -0,0 +1,35 @@
+namespace quanlykhachsan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2GradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2CirclePictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
